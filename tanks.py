dd366afeacf3f8529e2cb514365ec832268a6f91
macaroonie <nqn205@nyu.edu>
2018-10-27 16:23:31 -0400

    updated

--- tanks.py
+++ tanks.py
@@ -13,85 +13,7 @@ tank1Color = 'b'
 tank2Color = 'r'
 obstacleColor = 'k'
 
-##### functions provided to you #####
-def getNumberInput (prompt, validRange = [-np.Inf, np.Inf]):
-    """displays prompt and converts user input to a number
-    
-       in case of non-numeric input, re-prompts user for numeric input
-       
-       Parameters
-       ----------
-           prompt : str
-               prompt displayed to user
-           validRange : list, optional
-               two element list of form [min, max]
-               value entered must be in range [min, max] inclusive
-        Returns
-        -------
-            float
-                number entered by user
-    """
-    while True:
-        try:
-            num = float(input(prompt))
-        except Exception:
-            print ("Please enter a number")
-        else:
-            if (num >= validRange[0] and num <= validRange[1]):
-                return num
-            else:
-                print ("Please enter a value in the range [", validRange[0], ",", validRange[1], ")") #Python 3 sytanx
-            
-    return num    
 
-def showWindow():
-    """
-    shows the window -- call at end of drawBoard and tankShot
-    """
-    plt.draw()
-    plt.pause(0.001)
-    plt.show()
-
-
-def drawBox(box, color):
-    """
-    draws a filled box in the current axis
-    parameters
-    ----------
-    box : tuple
-        (left,right,bottom,top) - extents of the box
-     color : str
-        color to fill the box with, e.g. 'b'
-    """    
-    x = (box[0], box[0], box[1], box[1])
-    y = (box[2], box[3], box[3], box[2])
-    ax = plt.gca()
-    ax.fill(x,y, c = color)
-
-
-
-def endTrajectoryAtIntersection (x,y,box):
-    """
-    portion of trajectory prior to first intersection with box
-    
-    paramaters
-    ----------
-    x,y : np array type
-        position to check
-    box : tuple
-        (left,right,bottom,top)
-    
-    returns
-    ----------
-    (x,y) : tuple of np.array of floats
-        equal to inputs if (x,y) does not intersect box
-        otherwise returns the initial portion of the trajectory
-        up until the point of intersection with the box
-    """
-    i = firstInBox(x,y,box)
-    if (i < 0):
-        return (x,y)
-    return (x[0:i],y[0:i])
 
 
 ##### functions you need to implement #####
@@ -196,11 +118,14 @@ def tankShot (targetBox, obstacleBox, x0, y0, v, theta, g = 9.8):
     
     if firstInBox(x, y, obstacleBox) > -1: 
         plt.plot(x1, y1, color = 'k')
+        showWindow()
         return 0
     if firstInBox(x, y, targetBox) > -1:
         plt.plot(x, y, color = 'k')
+        showWindow()
         return 1
     plt.plot(x, y, color = 'k')
+    showWindow()
     return 0
 
         
@@ -225,7 +150,7 @@ def drawBoard (tank1box, tank2box, obstacleBox, playerNum):
     drawBox(obstacleBox, obstacleColor)
     drawBox(tank1box, tank1Color)
     drawBox(tank2box, tank2Color)
-    #print("It is player " + str(playerNum) + "'s turn to shoot." )
+    print("It is player " + str(playerNum) + "'s turn to shoot." )
     #your code here
     
     showWindow() #this makes the figure window show up
@@ -278,10 +203,6 @@ def oneTurn (tank1box, tank2box, obstacleBox, playerNum, g = 9.8):
     else:
         return playerNum
     
-obstacleBox = (40,60,0,50)
-tank1box = (10,15,0,5)
-tank2box = (90,95,0,5)
-oneTurn (tank1box, tank2box, obstacleBox, 1)
    
 
 
@@ -311,9 +232,88 @@ def playGame(tank1box, tank2box, obstacleBox, g = 9.8):
             playerNum = 2
         else:
             playerNum = 1
-    print("Congratulations player " + playerNum + "!")
-        
-#playGame(tank1box, tank2box, obstacleBox)
+    print("Congratulations player " + str(playerNum) + "!")
+ 
+       
+##### functions provided to you #####
+def getNumberInput (prompt, validRange = [-np.Inf, np.Inf]):
+    """displays prompt and converts user input to a number
+    
+       in case of non-numeric input, re-prompts user for numeric input
+       
+       Parameters
+       ----------
+           prompt : str
+               prompt displayed to user
+           validRange : list, optional
+               two element list of form [min, max]
+               value entered must be in range [min, max] inclusive
+        Returns
+        -------
+            float
+                number entered by user
+    """
+    while True:
+        try:
+            num = float(input(prompt))
+        except Exception:
+            print ("Please enter a number")
+        else:
+            if (num >= validRange[0] and num <= validRange[1]):
+                return num
+            else:
+                print ("Please enter a value in the range [", validRange[0], ",", validRange[1], ")") #Python 3 sytanx
+            
+    return num    
+
+def showWindow():
+    """
+    shows the window -- call at end of drawBoard and tankShot
+    """
+    plt.draw()
+    plt.pause(0.001)
+    plt.show()
+
+
+def drawBox(box, color):
+    """
+    draws a filled box in the current axis
+    parameters
+    ----------
+    box : tuple
+        (left,right,bottom,top) - extents of the box
+     color : str
+        color to fill the box with, e.g. 'b'
+    """    
+    x = (box[0], box[0], box[1], box[1])
+    y = (box[2], box[3], box[3], box[2])
+    ax = plt.gca()
+    ax.fill(x,y, c = color)
+
+
+
+def endTrajectoryAtIntersection (x,y,box):
+    """
+    portion of trajectory prior to first intersection with box
+    
+    paramaters
+    ----------
+    x,y : np array type
+        position to check
+    box : tuple
+        (left,right,bottom,top)
+    
+    returns
+    ----------
+    (x,y) : tuple of np.array of floats
+        equal to inputs if (x,y) does not intersect box
+        otherwise returns the initial portion of the trajectory
+        up until the point of intersection with the box
+    """
+    i = firstInBox(x,y,box)
+    if (i < 0):
+        return (x,y)
+    return (x[0:i],y[0:i])
         
         
     
@@ -332,5 +332,3 @@ def main():
 #don't edit the lines below;
 if __name__== "__main__":
     main()  
-        
-    
